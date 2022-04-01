ce4d5232d271b74085458c84050f20867b106b57
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-04-01 11:14:51 +0200

    add some print to debug a bit

--- tests/test_pyclesperanto.py
+++ tests/test_pyclesperanto.py
@@ -5,15 +5,25 @@ import pyclesperanto as cle
 def test_execute_kernel():
     # init gpu and print info
     gpu = cle.gpu()
+    gpu.set_wait_for_kernel_to_finish(True)
+    
     print(gpu.info())
+    
+    input = np.ones((3,3,3), dtype=np.float32)
+    valid = np.ones((3,3,3), dtype=np.float32) + 100
 
     # push and create buffer
-    out = gpu.create((5,5,5))
-    int = gpu.push(np.ones((5,5,5), dtype=np.float32))
+    out = gpu.create(input.shape)
+    int = gpu.push(input)
 
     # apply kernel
     cle.add_image_and_scalar(gpu, int, out, 100)
 
     # pull from device result and assert
     result = gpu.pull(out)
-    assert(np.sum(result.flatten()) == (result.size * 101))
+    
+    print("input:", input.flatten())
+    print("result:", result.flatten())
+    print("valid:", valid.flatten())
+
+    assert(np.sum(result.flatten()) == np.sum(valid.flatten()))

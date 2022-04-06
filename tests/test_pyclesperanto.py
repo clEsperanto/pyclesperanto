321b2e36134997e5c931da36e48f93056f699bca
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-04-06 15:52:30 +0200

    fixed tests

--- tests/test_pyclesperanto.py
+++ tests/test_pyclesperanto.py
@@ -1,38 +1,38 @@
 import time
 import numpy as np
-import pyclesperanto as cle
+from pyclesperanto import Clesperanto
 
 
 def test_gpu_info():
-    gpu = cle.gpu()
-    print(gpu.info())
+    cle = Clesperanto()
+    print(cle.info())
 
 
 def test_execute_kernel():
     # init gpu and print info
-    gpu = cle.gpu()
-    gpu.set_wait_for_kernel_to_finish()
+    cle = Clesperanto()
+    cle.set_wait_for_kernel_to_finish()
         
-    input = np.ones((3,3,1), dtype=np.float32)
+    input_image = np.ones((3,3,1), dtype=np.float32)
     valid = np.ones((3,3,1), dtype=np.float32) + 100
 
     # push and create buffer
-    out = gpu.create(input.shape)
-    int = gpu.push(input)
+    output_gpu = cle.create(input_image.shape)
+    input_gpu = cle.push(input_image)
     
     time.sleep(1)
 
     # apply kernel
-    cle.add_image_and_scalar(gpu, int, out, 100)
+    cle.add_image_and_scalar(input_gpu, output_gpu, 100)
     
     time.sleep(1)
 
     # pull from device result and assert
-    result = gpu.pull(out)
+    result = cle.pull(output_gpu)
     
     time.sleep(1)
     
-    print("input:", input.flatten(), input.dtype)
+    print("input:", input_image.flatten(), input_image.dtype)
     print("valid:", valid.flatten(), valid.dtype)
     print("result:", result.flatten(), result.dtype)
             

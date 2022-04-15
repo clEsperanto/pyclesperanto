85998a0337f2a7ebe9e22418b9b4176490064e94
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-04-15 11:17:54 +0200

    test if it works

--- tests/test_pyclesperanto.py
+++ tests/test_pyclesperanto.py
@@ -1,4 +1,3 @@
-import time
 import numpy as np
 from pyclesperanto import Clesperanto
 
@@ -13,27 +12,24 @@ def test_execute_kernel():
     cle = Clesperanto()
     cle.set_wait_for_kernel_to_finish()
         
-    input_image = np.ones((3,3,1), dtype=np.float32)
-    valid = np.ones((3,3,1), dtype=np.float32) + 100
+    input_image = np.ones((5,10), dtype=np.float32)
+    valid = np.ones((5,10), dtype=np.float32) + 100
 
     # push and create buffer
     output_gpu = cle.create(input_image.shape)
     input_gpu = cle.push(input_image)
     
-    time.sleep(1)
 
     # apply kernel
-    cle.add_image_and_scalar(input_gpu, output_gpu, 100)
+    cle.add_image_and_scalar(input_gpu, output_gpu, scalar=100)
     
-    time.sleep(1)
 
     # pull from device result and assert
-    result = cle.pull(output_gpu)
-    
-    time.sleep(1)
+    result = cle.pull(output_gpu)   
     
     print("input:", input_image.flatten(), input_image.dtype)
     print("valid:", valid.flatten(), valid.dtype)
     print("result:", result.flatten(), result.dtype)
             
     assert(np.sum(result.flatten()) == np.sum(valid.flatten()))
+

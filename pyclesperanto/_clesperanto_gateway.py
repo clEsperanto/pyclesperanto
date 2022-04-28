75a5497b55fb2ad45865d7feba73822ac359d5b8
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-04-28 13:37:56 +0200

    adding to gateway: imshow, maximum_z_projection, difference_of_gaussian

--- pyclesperanto/_clesperanto_gateway.py
+++ pyclesperanto/_clesperanto_gateway.py
@@ -58,7 +58,57 @@ class Clesperanto:
     def pull(self, np_array):
         return self._gpu.pull(np_array)
 
+    def imshow(self, image, title: str = None, labels: bool = False, min_display_intensity: float = None,
+               max_display_intensity: float = None, color_map=None, plot=None, colorbar: bool = False, colormap=None,
+               alpha: float = None, continue_drawing: bool = False):
+        import numpy as np
 
+        if not isinstance(image, np.ndarray):
+            # Todo: add self.pull here
+            image = self.pull(image)
+
+        if len(image.shape) == 3:
+            image = self.pull(self.maximum_z_projection(image))
+
+        if color_map is not None:
+            import warnings
+            warnings.warn("The imshow parameter color_map is deprecated. Use colormap instead.")
+            if colormap is None:
+                colormap = color_map
+
+        if colormap is None:
+            colormap = "Greys_r"
+
+        cmap = colormap
+        if labels:
+            import matplotlib
+            import numpy as np
+
+            if not hasattr(self, "labels_cmap"):
+                from numpy.random import MT19937
+                from numpy.random import RandomState, SeedSequence
+                rs = RandomState(MT19937(SeedSequence(3)))
+                lut = rs.rand(65537, 3)
+                lut[0, :] = 0
+                self.labels_cmap = matplotlib.colors.ListedColormap(lut)
+            cmap = self.labels_cmap
+
+            if min_display_intensity is None:
+                min_display_intensity = 0
+
+        if plot is None:
+            import matplotlib.pyplot as plt
+            plt.imshow(image, cmap=cmap, vmin=min_display_intensity, vmax=max_display_intensity,
+                       interpolation='nearest', alpha=alpha)
+            if colorbar:
+                plt.colorbar()
+            if not continue_drawing:
+                plt.show()
+        else:
+            plot.imshow(image, cmap=cmap, vmin=min_display_intensity, vmax=max_display_intensity,
+                        interpolation='nearest', alpha=alpha)
+            if colorbar:
+                plot.colorbar()
 
     @plugin_function
     def add_image_and_scalar(self, input_image: Image, output_image: Image = None, scalar: float = 0):
@@ -118,4 +168,40 @@ class Clesperanto:
     def histogram(self, input_image: Image, output_image: Image = None, bins : int = 256):
         from ._pyclesperanto import histogram as op
         op(self._gpu, input_image, output_image, bins)
+        return output_image
+
+    @plugin_function
+    def maximum_z_projection(self, input_image: Image, output_image: Image = None):
+        shape = input_image.shape()
+        output_image = self.create([shape[1], shape[2]])
+        from ._pyclesperanto import maximum_z_projection as op
+        op(self._gpu, input_image, output_image)
+        return output_image
+
+    @plugin_function
+    def maximum_x_projection(self, input_image: Image, output_image: Image = None):
+        shape = input_image.shape()
+        output_image = self.create([shape[1], shape[0]])
+        from ._pyclesperanto import maximum_x_projection as op
+        op(self._gpu, input_image, output_image)
+        return output_image
+
+    @plugin_function
+    def difference_of_gaussian(self, input_image: Image, output_image: Image = None,
+                               sigma1_x : float = 0,
+                               sigma1_y : float = 0,
+                               sigma1_z : float = 0,
+                               sigma2_x : float = 0,
+                               sigma2_y : float = 0,
+                               sigma2_z : float = 0,
+                               ):
+        from ._pyclesperanto import difference_of_gaussian as op
+        op(self._gpu, input_image, output_image,
+           float(sigma1_x),
+           float(sigma1_y),
+           float(sigma1_z),
+           float(sigma2_x),
+           float(sigma2_y),
+           float(sigma2_z),
+           )
         return output_image

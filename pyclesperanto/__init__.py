2910258819b0712138f3c86135005152fa2c6531
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-09-06 12:42:40 +0200

    use version.py

--- pyclesperanto/__init__.py
+++ pyclesperanto/__init__.py
@@ -1,10 +1,9 @@
-
-from pyclesperanto._pyclesperanto import __version__
+from pyclesperanto.version import VERSION, VERSION_STATUS, VERSION_TEXT 
 from pyclesperanto._pyclesperanto import pygateway
 
 # This supports ocl-icd find shipped OpenCL ICDs, cf. 
 import os
 os.environ["PYCLESPERANTO_HOME"] = os.path.dirname(os.path.abspath(__file__))
 
-print("clesperanto version:",__version__)
+print("clesperanto version:",VERSION)
 cle = pygateway()
--- wrapper/pyclesperanto.cpp
+++ wrapper/pyclesperanto.cpp
@@ -1,17 +1,9 @@
 
 #include "pyclesperanto.hpp"
 
-#define STRINGIFY(x) #x
-#define MACRO_STRINGIFY(x) STRINGIFY(x)
-
 PYBIND11_MODULE(_pyclesperanto, m) {
   // define a module. module name = file name = cmake target name
   init_pygateway(m);
   init_pyimage(m);
 
-#ifdef VERSION_INFO
-  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-  m.attr("__version__") = "dev";
-#endif
 }

c15e0e68412df3ec9e91a0388abdad80d5878d3b
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-09-30 14:04:15 +0200

    FIX: wrapping order matters

--- wrapper/pyclesperanto.cpp
+++ wrapper/pyclesperanto.cpp
@@ -4,7 +4,7 @@
 PYBIND11_MODULE(_pyclesperanto, m)
 {
   // define a module. module name = file name = cmake target name
-  init_clgateway(m);
-  init_climage(m);
   init_cltypes(m);
+  init_climage(m);
+  init_clgateway(m);
 }

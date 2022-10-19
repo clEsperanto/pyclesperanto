3932a818c0a7bf09b18999c709417e70e07d8fa8
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-10-19 17:18:40 +0200

    FIX: print pyclesperanto instead of clesperanto

--- pyclesperanto/__init__.py
+++ pyclesperanto/__init__.py
@@ -4,14 +4,14 @@ from ._pyclesperanto import _ListAvailableDevices
 from ._pyclesperanto import _AbsoluteKernel_Call
 
 from pyclesperanto import _gateway, _types, _version
-from pyclesperanto._version import VERSION, VERSION_STATUS, VERSION_TEXT
-from pyclesperanto._gateway import Clesperanto
-from pyclesperanto._types import cleImage, mType, dType
+from ._version import VERSION, VERSION_STATUS, VERSION_TEXT
+from ._gateway import Clesperanto
+from ._types import cleImage, mType, dType
 
 # This supports ocl-icd find shipped OpenCL ICDs, cf.
 import os
 
 os.environ["PYCLESPERANTO_HOME"] = os.path.dirname(os.path.abspath(__file__))
 
-print("clesperanto version:", VERSION_TEXT)
+print("pyclesperanto version:", VERSION_TEXT)
 cle = Clesperanto()

4a6a1e33717c5153b26421dcd722283562cf57ba
StRigaud <rigaud.stephane@gmail.com>
2022-11-17 16:39:59 +0100

    add max y proj

--- pyclesperanto/_tier1.py
+++ pyclesperanto/_tier1.py
@@ -72,6 +72,22 @@ def maximum_z_projection(
 
 
 @plugin_function
+def maximum_y_projection(
+    input_image: Image,
+    output_image: Image = None,
+    device: cleDevice = None,
+) -> Image:
+
+    from ._pyclesperanto import _MaximumYProjectionKernel_Call as op
+
+    if device is None:
+        device = get_device()
+
+    op(device, input_image, output_image)
+    return output_image
+
+
+@plugin_function
 def threshold_otsu(
     input_image: Image,
     output_image: Image = None,

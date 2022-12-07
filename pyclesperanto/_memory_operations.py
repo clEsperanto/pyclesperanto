d204c3b5c284b8b72474a1ee4c8d1a3fb97ed8f0
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2022-12-07 11:31:38 +0100

    ADD: create, push, pull dtype dependent

--- pyclesperanto/_memory_operations.py
+++ pyclesperanto/_memory_operations.py
@@ -1,10 +1,52 @@
 import numpy as np
-from ._pyclesperanto import _Create, _Push, _Pull
+from ._pyclesperanto import (
+    _Create,
+    _PushFloat,
+    _PullFloat,
+    _PushInt32,
+    _PullInt32,
+    _PushInt16,
+    _PullInt16,
+    _PushInt8,
+    _PullInt8,
+    _PushUint32,
+    _PullUint32,
+    _PushUint16,
+    _PullUint16,
+    _PushUint8,
+    _PullUint8,
+)
 from ._device import Device, get_device
-from ._image import cleImage, Image, mType
+from ._image import cleImage, Image, mType, dType
 
 
-def create(shape: tuple, mtype: mType = None, device: Device = None) -> Image:
+# def create(shape: tuple, mtype: mType = None, device: Device = None) -> Image:
+#     """create:
+
+#     Conventional method to create images on the GPU and return its handle
+
+#     Parameters
+#     ----------
+#     shape : tuple
+#         shape of the memory to create, using the zyx convention
+#     mtype : mType, optional
+#         buffer or image structure
+
+#     Returns
+#     -------
+#     Image
+#         Handle of the empty GPU image
+#     """
+#     if mtype is None:
+#         mtype = mType.buffer
+#     if device is None:
+#         device = get_device()
+#     return cleImage(_Create(device, shape, mtype))
+
+
+def create(
+    shape: tuple, dtype: dType = None, mtype: mType = None, device: Device = None
+) -> Image:
     """create:
 
     Conventional method to create images on the GPU and return its handle
@@ -13,19 +55,23 @@ def create(shape: tuple, mtype: mType = None, device: Device = None) -> Image:
     ----------
     shape : tuple
         shape of the memory to create, using the zyx convention
+    dtype : dType, optional
+        data type of the memory to create (float, int, etc.)
     mtype : mType, optional
-        buffer or image structure
+        memory structure of the memory to create (buffer or image)
 
     Returns
     -------
     Image
-        Handle of the empty GPU image
+        Handle of the empty GPU image (buffer or image) with the specified data type
     """
+    if dtype is None:
+        dtype = dType.float
     if mtype is None:
         mtype = mType.buffer
     if device is None:
         device = get_device()
-    return cleImage(_Create(device, shape, mtype))
+    return cleImage(_Create(device, shape, dtype, mtype))
 
 
 def create_like(image: Image) -> Image:
@@ -45,9 +91,15 @@ def create_like(image: Image) -> Image:
         Handle of the empty GPU image
     """
     if isinstance(image, cleImage):
-        return create(shape=tuple(image.shape), mtype=image.mtype, device=image.device)
+        return create(
+            shape=tuple(image.shape),
+            dtype=image.dtype,
+            mtype=image.mtype,
+            device=image.device,
+        )
     else:
-        return create(shape=tuple(image.shape), mtype=mType.buffer)
+        # todo: get dtype from numpy array and convert to cle dType
+        return create(shape=tuple(image.shape))
 
 
 def push(array: Image, mtype: mType = None, device: Device = None) -> Image:
@@ -60,21 +112,51 @@ def push(array: Image, mtype: mType = None, device: Device = None) -> Image:
     array : Image (numpy array, etc.)
         Can be of other type compatible with numpy
     mtype : mType, optional
-        buffer or image memory structure structure
+        memory structure of the memory to create (buffer or image)
 
     Returns
     -------
     Image
-        Handle of the GPU image
+        Handle of the GPU image (buffer or image) with the specified data type
     """
     if isinstance(array, cleImage):
         return array
     else:
-        if mtype is None:
-            mtype = mType.buffer
         if device is None:
             device = get_device()
-        return cleImage(_Push(device, np.asarray(array), mtype))
+        if mtype is None:
+            mtype = mType.buffer
+
+        if array.dtype == np.float32:
+            return cleImage(
+                _PushFloat(device, np.asarray(array, dtype=np.float32), mtype)
+            )
+        elif array.dtype == np.int32:
+            return cleImage(
+                _PushInt32(device, np.asarray(array, dtype=np.int32), mtype)
+            )
+        elif array.dtype == np.int16:
+            return cleImage(
+                _PushInt16(device, np.asarray(array, dtype=np.int16), mtype)
+            )
+        elif array.dtype == np.int8:
+            return cleImage(_PushInt8(device, np.asarray(array, dtype=np.int8), mtype))
+        elif array.dtype == np.uint32:
+            return cleImage(
+                _PushUint32(device, np.asarray(array, dtype=np.uint32), mtype)
+            )
+        elif array.dtype == np.uint16:
+            return cleImage(
+                _PushUint16(device, np.asarray(array, dtype=np.uint16), mtype)
+            )
+        elif array.dtype == np.uint8:
+            return cleImage(
+                _PushUint8(device, np.asarray(array, dtype=np.uint8), mtype)
+            )
+        else:
+            return cleImage(
+                _PushFloat(device, np.asarray(array, dtype=np.float32), mtype)
+            )
 
 
 def pull(image: Image) -> Image:
@@ -93,6 +175,21 @@ def pull(image: Image) -> Image:
         numpy compatible array
     """
     if isinstance(image, cleImage):
-        return _Pull(image)
+        if image.dtype == dType.float:
+            return _PullFloat(image)
+        elif image.dtype == dType.int32:
+            return _PullInt32(image)
+        elif image.dtype == dType.int16:
+            return _PullInt16(image)
+        elif image.dtype == dType.int8:
+            return _PullInt8(image)
+        elif image.dtype == dType.uint32:
+            return _PullUint32(image)
+        elif image.dtype == dType.uint16:
+            return _PullUint16(image)
+        elif image.dtype == dType.uint8:
+            return _PullUint8(image)
+        else:
+            return _PullFloat(image)
     else:
         return image

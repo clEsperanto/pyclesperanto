09a17f41094fe1de080abe01d7d87130cff1167e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-01-06 18:29:07 +0100

    STYLE: refactor code

--- wrapper/cleImage.cpp
+++ wrapper/cleImage.cpp
@@ -31,15 +31,6 @@ auto init_cleimage(pybind11::module_ &m) -> void
   object.def("Fill", &cle::Image::Fill, "", pybind11::arg("value"));
   object.def("CopyDataTo", &cle::Image::CopyDataTo, "", pybind11::arg("image"));
   object.def(
-      "__str__", [](const cle::Image &image)
-      {
-                 std::stringstream out_string;
-                 out_string << "<cle::Image " << cle::MemoryTypeToString(image.GetMemoryType());
-                 out_string << "("<<cle::DataTypeToString(image.GetDataType()) <<"),";
-                 out_string << "shape=(" << image.Shape()[2] << ", " << image.Shape()[1] << ", " << image.Shape()[0] <<")>";
-                 return out_string.str(); },
-      "");
-  object.def(
       "__repr__", [](const cle::Image &image)
       {
                  std::stringstream out_string;
@@ -51,23 +42,16 @@ auto init_cleimage(pybind11::module_ &m) -> void
   object.def(
       "__len__", [](const cle::Image &image)
       {
-                 if (image.Shape()[2] > 1)
-                 {
-                   return image.Shape()[2];
-                 }
-                 if(image.Shape()[1] > 1 )
-                 {
-                   return image.Shape()[1];
-                 }
-                 return image.Shape()[0]; },
+    switch(image.Ndim())
+    {
+    case 1:
+      return image.Shape()[0];
+    case 2:
+      return image.Shape()[1];
+    case 3:
+      return image.Shape()[2];
+    } },
       "");
-
-  // object.__lt__(self, other)
-  // object.__le__(self, other)
-  // object.__eq__(self, other)
-  // object.__ne__(self, other)
-  // object.__gt__(self, other)
-  // object.__ge__(self, other)
 
   object.doc() = R"pbdoc(
         clImage class wrapper

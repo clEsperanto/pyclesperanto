0768d765625186eda8a2d8e9c73c12dd79294189
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-01-09 16:35:05 +0100

    Back to previous CI

--- tests/test_run_kernel.py
+++
@@ -1,9 +0,0 @@
-def test_run():
-    import pyclesperanto as cle
-    import numpy as np
-
-    rand_arr = np.ones((100, 100)).astype(np.uint8)
-    binary = cle.add_image_and_scalar(rand_arr, scalar=10)
-    res = cle.pull(binary)
-
-    assert res.sum() == (rand_arr + 10).sum()

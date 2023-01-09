aef8a0f3956ab407f04d50db55340d3996565862
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-01-09 15:51:26 +0100

    TEST: try to fix pytest

--- tests/test_pyclesperanto.py
+++ tests/test_pyclesperanto.py
@@ -1,4 +1,7 @@
 def test_init():
     import pyclesperanto as cle
 
+    print("pyclesperanto version:", cle.__version__)
+    print("relying on CLIc version:", cle.__clic_version__)
+
     print(cle.list_available_devices())

8d50b69100e99b491983e41b66c53b09f26a2056
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-03-22 10:37:05 +0100

    add a test for testing

---
+++ tests/test_connected_components_labeling_box.py
@@ -0,0 +1,29 @@
+def test_connected_components_labeling_box():
+    import pyclesperanto as cle
+    import numpy as np
+    
+    gpu_input = cle.push(np.asarray([
+        [
+            [1, 0, 1],
+            [1, 0, 0],
+            [0, 0, 1]
+        ]
+    ]))
+
+    gpu_reference = cle.push(np.asarray([
+        [
+            [1, 0, 2],
+            [1, 0, 0],
+            [0, 0, 3]
+        ]
+    ]))
+
+    gpu_output = cle.connected_components_labeling_box(gpu_input)
+
+    a = cle.pull(gpu_output)
+    b = cle.pull(gpu_reference)
+
+    print(a)
+    print(b)
+
+    assert (np.array_equal(a, b))

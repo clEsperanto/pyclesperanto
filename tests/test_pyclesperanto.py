af35933e73578089ae00d2d9f7b0a3e6b9f09872
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-03-23 11:50:55 +0100

    RM: tests

--- tests/test_pyclesperanto.py
+++
@@ -1,7 +0,0 @@
-def test_init():
-    import pyclesperanto as cle
-
-    print("pyclesperanto version:", cle.__version__)
-    print("relying on CLIc version:", cle.__clic_version__)
-
-    print(cle.list_available_devices())

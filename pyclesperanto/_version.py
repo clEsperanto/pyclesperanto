9fc36bdb5e18f472de423873dd9a617f6a508a4f
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-04-20 11:07:50 +0200

    Update clic version

--- pyclesperanto/_version.py
+++ pyclesperanto/_version.py
@@ -4,6 +4,6 @@ VERSION_STATUS = ""
 VERSION = ".".join(str(x) for x in VERSION_CODE) + VERSION_STATUS
 
 # clic version
-CLIC_VERSION_CODE = (0, 6, 3)
+CLIC_VERSION_CODE = (0, 6, 4)
 CLIC_VERSION_STATUS = ""
 CLIC_VERSION = ".".join(str(x) for x in CLIC_VERSION_CODE) + CLIC_VERSION_STATUS

ce1623cf400d7a7f7a1bf7b792031c1ea4731c51
Stephane Rigaud <Stephane.rigaud@pasteur.fr>
2023-07-17 19:58:17 +0000

    test windows

--- scripts/build-opencl-windows.sh
+++ scripts/build-opencl-windows.sh
@@ -6,7 +6,7 @@ set -o xtrace
 git clone --branch v2022.01.04 https://github.com/KhronosGroup/OpenCL-ICD-Loader
 git clone --branch v2022.01.04 https://github.com/KhronosGroup/OpenCL-Headers
 
-cmake -D CMAKE_INSTALL_PREFIX=./OpenCL-Headers/install -S ./OpenCL-Headers -B ./OpenCL-Headers/build 
+cmake -D CMAKE_INSTALL_PREFIX=./OpenCL-ICD-Loader/inc -S ./OpenCL-Headers -B ./OpenCL-Headers/build 
 cmake --build ./OpenCL-Headers/build --target install
 
 # if someone would like to try to create win32 wheels bellow lines may be useful

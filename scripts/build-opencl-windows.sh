bf768afd10f009ab1b6a43e763613865836d02cf
Stephane Rigaud <Stephane.rigaud@pasteur.fr>
2023-07-17 19:48:10 +0000

    test windows build

--- scripts/build-opencl-windows.sh
+++ scripts/build-opencl-windows.sh
@@ -4,9 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 set -o xtrace
 
 git clone --branch v2022.01.04 https://github.com/KhronosGroup/OpenCL-ICD-Loader
-
 git clone --branch v2022.01.04 https://github.com/KhronosGroup/OpenCL-Headers
-
 
 cmake -D CMAKE_INSTALL_PREFIX=./OpenCL-Headers/install -S ./OpenCL-Headers -B ./OpenCL-Headers/build 
 cmake --build ./OpenCL-Headers/build --target install
@@ -15,8 +13,11 @@ cmake --build ./OpenCL-Headers/build --target install
 # cmake -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -DOPENCL_ICD_LOADER_HEADERS_DIR=${PWD}/OpenCL-Headers/install/include -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build 
 # cmake --build ./OpenCL-ICD-Loader/build --target install --config Release
 
-cmake -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -D OPENCL_ICD_LOADER_HEADERS_DIR=${PWD}/OpenCL-Headers/install/include -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build2 -A x64
-cmake --build ./OpenCL-ICD-Loader/build2 --target install --config Release
+ cmake -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -D CMAKE_INSTALL_PREFIX=./OpenCL-ICD-Loader/install -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build 
+ cmake --build ./OpenCL-ICD-Loader/build --target install
+
+# cmake -D CMAKE_PREFIX_PATH=${PWD}/OpenCL-Headers/install -D OPENCL_ICD_LOADER_HEADERS_DIR=${PWD}/OpenCL-Headers/install/include -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build2 -A x64
+# cmake --build ./OpenCL-ICD-Loader/build2 --target install --config Release
 
 # why not had c++ binding in the mix?
 

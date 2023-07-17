89d77d0d5b811badc5ac290ea9715c6677445885
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-17 12:53:48 +0200

    test again

--- setup.py
+++ setup.py
@@ -33,8 +33,22 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     packages=["pyclesperanto"],
-    package_data={"pyclesperanto": ["demos/*", "docs/*", "developer_docs/*", "benchmarking/*"]},
-    exclude_package_data={"pyclesperanto": ["demos/*", "docs/*", "developer_docs/*", "benchmarking/*"]},
+    package_data={
+        'pyclesperanto': [
+            'demos/*',
+            'docs/*',
+            'developer_docs/*',
+            'benchmarking/*'
+        ]
+    },
+    exclude_package_data={
+        'pyclesperanto': [
+            'demos/*',
+            'docs/*',
+            'developer_docs/*',
+            'benchmarking/*'
+        ]
+    },
     cmake_install_dir="pyclesperanto",
     python_requires=">=3.7",
     install_requires=[

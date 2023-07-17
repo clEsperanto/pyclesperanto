d91e6d603047ad7b986aaae3849de38783ff0e6a
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-17 12:15:16 +0200

    Add .so per build type

---
+++ scripts/cuda_build.cmake
@@ -0,0 +1,18 @@
+message(STATUS "# Building CUDA version -------")
+
+set(CY_CUDA_PACKAGE_NAME _cuda_clesperanto)
+
+set(BUILD_CUDA_BACKEND ON)
+set(BUILD_OCL_BACKEND OFF)
+find_package(CUDAToolkit REQUIRED)
+
+find_package(pybind11 CONFIG REQUIRED)
+pybind11_add_module(${CY_CUDA_PACKAGE_NAME} MODULE ${WRAPPER_SOURCES_FILES})
+
+target_link_libraries(${CY_CUDA_PACKAGE_NAME} PRIVATE CLIc::CLIc)
+add_dependencies(${CY_CUDA_PACKAGE_NAME} CLIc)
+target_include_directories(${CY_CUDA_PACKAGE_NAME} PUBLIC "$<BUILD_INTERFACE:${WRAPPER_DIR}>")
+target_compile_features(${CY_CUDA_PACKAGE_NAME} PRIVATE cxx_std_17)
+
+# Installing the extension module to the root of the package
+install(TARGETS ${CY_CUDA_PACKAGE_NAME} DESTINATION .)
---
+++ scripts/ocl_build.cmake
@@ -0,0 +1,22 @@
+message(STATUS "# Building OCL version --------")
+
+set(CY_OCL_PACKAGE_NAME _ocl_clesperanto)
+
+set(BUILD_CUDA_BACKEND OFF)
+set(BUILD_OCL_BACKEND ON)
+if (DEFINED OpenCL_LIBRARIES AND DEFINED OpenCL_INCLUDE_DIRS)
+    set(OpenCL_FOUND true)
+else()
+    find_package(OpenCL REQUIRED)
+endif()
+
+find_package(pybind11 CONFIG REQUIRED)
+pybind11_add_module(${CY_OCL_PACKAGE_NAME} MODULE ${WRAPPER_SOURCES_FILES})
+
+target_link_libraries(${CY_OCL_PACKAGE_NAME} PRIVATE CLIc::CLIc)
+add_dependencies(${CY_OCL_PACKAGE_NAME} CLIc)
+target_include_directories(${CY_OCL_PACKAGE_NAME} PUBLIC "$<BUILD_INTERFACE:${WRAPPER_DIR}>")
+target_compile_features(${CY_OCL_PACKAGE_NAME} PRIVATE cxx_std_17)
+
+# Installing the extension module to the root of the package
+install(TARGETS ${CY_OCL_PACKAGE_NAME} DESTINATION .)

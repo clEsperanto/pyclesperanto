6a3fd2f85a617624dc2479a7af624e14dea39744
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-17 12:15:42 +0200

    manage import incompatibility

--- pyclesperanto/__init__.py
+++ pyclesperanto/__init__.py
@@ -2,10 +2,22 @@
 from ._version import VERSION as __version__
 from ._version import CLIC_VERSION as __clic_version__
 
+try:
+    from . import _clesperanto as _cle
+except ImportError:
+    try:
+        from . import _ocl_clesperanto as _cle
+    except ImportError:
+        try:
+            from . import _cuda_clesperanto as _cle
+        except ImportError:
+            raise ImportError("Failed to import the backend")
+
 from ._types import (
     MemoryType,
     DataType,
 )
+
 from ._core import (
     select_backend,
     get_device,
@@ -26,8 +38,8 @@ import os
 # Supports for ocl-icd find shipped OpenCL ICDs, cf.
 os.environ["PYCLESPERANTO_HOME"] = os.path.dirname(os.path.abspath(__file__))
 
-# redefine memory type for easy usage
-buffer = MemoryType.buffer()
-image = MemoryType.image()
+# # redefine memory type for easy usage
+# buffer = MemoryType.buffer()
+# image = MemoryType.image()
 
 __common_alias__ = "cle"

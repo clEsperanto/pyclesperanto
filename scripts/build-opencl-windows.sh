1779d8800afe25c7e6a26f1b2fb0fe730a164cf2
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-18 10:49:09 +0200

    test program-file install

--- scripts/build-opencl-windows.sh
+++ scripts/build-opencl-windows.sh
@@ -4,17 +4,18 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 VERSION="v2022.01.04"
 OPENCL_HEADERS_DIR="${SCRIPT_DIR}/OpenCL-Headers"
 OPENCL_ICD_LOADER_DIR="${SCRIPT_DIR}/OpenCL-ICD-Loader"
+INSTALL_PREFIX="C:/Program Files/OpenCL-ICD-Loader"
 
 set -e
 
 # Clone and install OpenCL-Headers
 git clone --branch ${VERSION} https://github.com/KhronosGroup/OpenCL-Headers "${OPENCL_HEADERS_DIR}"
-cmake -D CMAKE_INSTALL_PREFIX="${OPENCL_HEADERS_DIR}/install" -S "${OPENCL_HEADERS_DIR}" -B "${OPENCL_HEADERS_DIR}/build"
+cmake -D CMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}/include" -S "${OPENCL_HEADERS_DIR}" -B "${OPENCL_HEADERS_DIR}/build"
 cmake --build "${OPENCL_HEADERS_DIR}/build" --target install
 
 # Clone and install OpenCL-ICD-Loader
 git clone --branch ${VERSION} https://github.com/KhronosGroup/OpenCL-ICD-Loader "${OPENCL_ICD_LOADER_DIR}"
-cmake -D CMAKE_INSTALL_PREFIX="${OPENCL_ICD_LOADER_DIR}/install" -D OPENCL_ICD_LOADER_HEADERS_DIR="${OPENCL_HEADERS_DIR}/install/include" -S "${OPENCL_ICD_LOADER_DIR}" -B "${OPENCL_ICD_LOADER_DIR}/build"
+cmake -D CMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}/lib" -D OPENCL_ICD_LOADER_HEADERS_DIR="${INSTALL_PREFIX}/include" -S "${OPENCL_ICD_LOADER_DIR}" -B "${OPENCL_ICD_LOADER_DIR}/build"
 cmake --build "${OPENCL_ICD_LOADER_DIR}/build" --target install --config Release
 
 # Append license information to your project's LICENSE file

adca3b45f93b70e1dcb7215acb1408a83d76548c
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-20 15:14:14 +0200

    update array wrapper

--- wrapper/array_.cpp
+++ wrapper/array_.cpp
@@ -2,6 +2,7 @@
 #include "pycle_wrapper.hpp"
 
 #include "array.hpp"
+#include "utils.hpp"
 
 #include <pybind11/functional.h>
 #include <pybind11/numpy.h>
@@ -11,19 +12,19 @@
 namespace py = pybind11;
 
 template <typename T>
-py::array_t<T> get(const cle::Array &array)
+py::array_t<T, py::array::c_style> read(const cle::Array &array)
 {
-     py::array_t<T> np_array;
+     py::array_t<T, py::array::c_style> np_array;
      switch (array.dim())
      {
      case 1:
-          np_array.resize({array.width()});
+          np_array = py::array_t<T, py::array::c_style>({array.width()});
           break;
      case 2:
-          np_array.resize({array.height(), array.width()});
+          np_array = py::array_t<T, py::array::c_style>({array.height(), array.width()});
           break;
      case 3:
-          np_array.resize({array.depth(), array.height(), array.width()});
+          np_array = py::array_t<T, py::array::c_style>({array.depth(), array.height(), array.width()});
           break;
      }
      py::buffer_info info = np_array.request();
@@ -34,7 +35,7 @@ py::array_t<T> get(const cle::Array &array)
 }
 
 template <typename T>
-void set(cle::Array &array, const py::array_t<T> &np_array)
+void write(cle::Array &array, const py::array_t<T, py::array::c_style> &np_array)
 {
      py::buffer_info info = np_array.request();
      const void *data = info.ptr;
@@ -42,38 +43,176 @@ void set(cle::Array &array, const py::array_t<T> &np_array)
      array.write(data);
 }
 
+py::object get_np_dtype(const cle::Array::Pointer &array)
+{
+     switch (array->dtype())
+     {
+     case cle::dType::FLOAT:
+          return py::dtype::of<float>();
+     case cle::dType::INT64:
+          return py::dtype::of<int64_t>();
+     case cle::dType::INT32:
+          return py::dtype::of<int>();
+     case cle::dType::INT16:
+          return py::dtype::of<int16_t>();
+     case cle::dType::INT8:
+          return py::dtype::of<int8_t>();
+     case cle::dType::UINT64:
+          return py::dtype::of<uint64_t>();
+     case cle::dType::UINT32:
+          return py::dtype::of<uint32_t>();
+     case cle::dType::UINT16:
+          return py::dtype::of<uint16_t>();
+     case cle::dType::UINT8:
+          return py::dtype::of<uint8_t>();
+     default:
+          throw std::invalid_argument("Invalid dType value");
+     }
+}
+
+cle::dType get_cle_dtype(const py::dtype &type)
+{
+     if (type.is(py::dtype::of<float>()))
+     {
+          return cle::dType::FLOAT;
+     }
+     else if (type.is(py::dtype::of<int64_t>()))
+     {
+          return cle::dType::INT64;
+     }
+     else if (type.is(py::dtype::of<int32_t>()))
+     {
+          return cle::dType::INT32;
+     }
+     else if (type.is(py::dtype::of<int16_t>()))
+     {
+          return cle::dType::INT16;
+     }
+     else if (type.is(py::dtype::of<int8_t>()))
+     {
+          return cle::dType::INT8;
+     }
+     else if (type.is(py::dtype::of<uint64_t>()))
+     {
+          return cle::dType::UINT64;
+     }
+     else if (type.is(py::dtype::of<uint32_t>()))
+     {
+          return cle::dType::UINT32;
+     }
+     else if (type.is(py::dtype::of<uint16_t>()))
+     {
+          return cle::dType::UINT16;
+     }
+     else if (type.is(py::dtype::of<uint8_t>()))
+     {
+          return cle::dType::UINT8;
+     }
+     else
+     {
+          throw std::invalid_argument("Invalid dtype value");
+     }
+}
+
+std::string get_str_mtype(const cle::Array::Pointer &array)
+{
+     switch (array->mtype())
+     {
+     case cle::mType::BUFFER:
+          return "buffer";
+     case cle::mType::IMAGE:
+          return "image";
+     default:
+          throw std::invalid_argument("Invalid mtype value");
+     }
+}
+
+cle::mType get_cle_mtype(const std::string &mtype)
+{
+     if (mtype == "buffer")
+     {
+          return cle::mType::BUFFER;
+     }
+     else if (mtype == "image")
+     {
+          return cle::mType::IMAGE;
+     }
+     else
+     {
+          throw std::invalid_argument("Invalid mtype value");
+     }
+}
+
+py::tuple get_np_shape(const cle::Array::Pointer &array)
+{
+     switch (array->dim())
+     {
+     case 1:
+          return py::make_tuple(array->width());
+     case 2:
+          return py::make_tuple(array->height(), array->width());
+     case 3:
+          return py::make_tuple(array->depth(), array->height(), array->width());
+     default:
+          throw std::invalid_argument("Invalid dimension value");
+     }
+}
+
+cle::Array::Pointer create_array(py::tuple shape, py::dtype dtype, std::string mtype, cle::Device::Pointer device)
+{
+     size_t width(1), height(1), depth(1);
+     switch (py::len(shape))
+     {
+     case 1:
+          width = shape[0].cast<size_t>();
+          break;
+     case 2:
+          height = shape[0].cast<size_t>();
+          width = shape[1].cast<size_t>();
+          break;
+     case 3:
+          depth = shape[0].cast<size_t>();
+          height = shape[1].cast<size_t>();
+          width = shape[2].cast<size_t>();
+          break;
+     default:
+          throw std::invalid_argument("Invalid dimension value");
+     }
+     auto array = cle::Array::create(width, height, depth, get_cle_dtype(dtype), get_cle_mtype(mtype), device);
+     return array;
+}
+
 auto array_(py::module_ &m) -> void
 {
      py::class_<cle::Array, std::shared_ptr<cle::Array>>(m, "_Array")
-         .def(py::init<cle::Array const &>())
-         .def_static("create",
-                     py::overload_cast<const size_t &, const size_t &, const size_t &, const cle::dType &, const cle::mType &, const cle::Device::Pointer &>(&cle::Array::create), py::return_value_policy::take_ownership, py::arg("width"), py::arg("height"), py::arg("depth"), py::arg("dtype"), py::arg("mtype"), py::arg("device"))
+         //     .def(py::init<cle::Array const &>())
+         //     .def_static("create",
+         //                 py::overload_cast<const size_t &, const size_t &, const size_t &, const cle::dType &, const cle::mType &, const cle::Device::Pointer &>(&cle::Array::create), py::return_value_policy::take_ownership, py::arg("width"), py::arg("height"), py::arg("depth"), py::arg("dtype"), py::arg("mtype"), py::arg("device"))
+         .def_static("create", &create_array, py::arg("shape"), py::arg("dtype"), py::arg("mtype"), py::arg("device"))
 
-         .def("set", set<float>)
-         .def("set", set<int8_t>)
-         .def("set", set<int16_t>)
-         .def("set", set<int32_t>)
-         .def("set", set<int64_t>)
-         .def("set", set<uint8_t>)
-         .def("set", set<uint16_t>)
-         .def("set", set<uint32_t>)
-         .def("set", set<uint64_t>)
+         .def("_write", &write<float>)
+         .def("_write", &write<int8_t>)
+         .def("_write", &write<int16_t>)
+         .def("_write", &write<int32_t>)
+         .def("_write", &write<int64_t>)
+         .def("_write", &write<uint8_t>)
+         .def("_write", &write<uint16_t>)
+         .def("_write", &write<uint32_t>)
+         .def("_write", &write<uint64_t>)
 
-         .def("get_float32", get<float>, py::return_value_policy::take_ownership)
-         .def("get_int8", get<int8_t>, py::return_value_policy::take_ownership)
-         .def("get_int16", get<int16_t>, py::return_value_policy::take_ownership)
-         .def("get_int32", get<int32_t>, py::return_value_policy::take_ownership)
-         .def("get_int64", get<int64_t>, py::return_value_policy::take_ownership)
-         .def("get_uint8", get<uint8_t>, py::return_value_policy::take_ownership)
-         .def("get_uint16", get<uint16_t>, py::return_value_policy::take_ownership)
-         .def("get_uint32", get<uint32_t>, py::return_value_policy::take_ownership)
-         .def("get_uint64", get<uint64_t>, py::return_value_policy::take_ownership)
+         .def("_read_float32", &read<float>)
+         .def("_read_int8", &read<int8_t>)
+         .def("_read_int16", &read<int16_t>)
+         .def("_read_int32", &read<int32_t>)
+         .def("_read_int64", &read<int64_t>)
+         .def("_read_uint8", &read<uint8_t>)
+         .def("_read_uint16", &read<uint16_t>)
+         .def("_read_uint32", &read<uint32_t>)
+         .def("_read_uint64", &read<uint64_t>)
 
          .def("copy", &cle::Array::copy, py::arg("dst"))
          .def("fill", &cle::Array::fill, py::arg("value"))
 
-         .def_property_readonly("dtype", &cle::Array::dtype)
-         .def_property_readonly("mtype", &cle::Array::mtype)
          .def_property_readonly("width", &cle::Array::width)
          .def_property_readonly("height", &cle::Array::height)
          .def_property_readonly("depth", &cle::Array::depth)
@@ -81,18 +220,10 @@ auto array_(py::module_ &m) -> void
          .def_property_readonly("itemsize", &cle::Array::bytesPerElements)
          .def_property_readonly("device", &cle::Array::device)
          .def_property_readonly("ndim", &cle::Array::dim)
+         .def_property_readonly("mtype", [](const cle::Array::Pointer &array)
+                                { return get_str_mtype(array); })
+         .def_property_readonly("dtype", [](const cle::Array::Pointer &array)
+                                { return get_np_dtype(array); })
          .def_property_readonly("shape", [](const cle::Array::Pointer &array)
-                                {
-          switch (array->dim())
-          {
-          case 1:
-               return py::make_tuple(array->width());
-               break;
-          case 2:
-               return py::make_tuple(array->height(), array->width());
-               break;
-          case 3:
-               return py::make_tuple(array->depth(), array->height(), array->width());
-               break;
-          } });
+                                { return get_np_shape(array); });
 }

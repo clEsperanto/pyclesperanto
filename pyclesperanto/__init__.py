f02b53e254a18cfd05c98388d09796f9a6ba6398
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-24 15:48:54 +0200

    clean up init and build

--- pyclesperanto/__init__.py
+++ pyclesperanto/__init__.py
@@ -17,12 +17,6 @@ from ._tier3 import *
 from ._tier4 import *
 from ._tier5 import *
 
-# # Generic Import
-# import os
-
-# # Supports for ocl-icd find shipped OpenCL ICDs, cf.
-# os.environ["PYCLESPERANTO_HOME"] = os.path.dirname(os.path.abspath(__file__))
-
 from ._version import VERSION as __version__
 from ._version import CLIC_VERSION as __clic_version__
 from ._version import COMMON_ALIAS as __common_alias__

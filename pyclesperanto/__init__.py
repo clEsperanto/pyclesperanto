87458181a1a1bc2eb938cf3ffd179736de228fc6
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-07-24 12:33:13 +0200

    silence os path declaration

--- pyclesperanto/__init__.py
+++ pyclesperanto/__init__.py
@@ -1,6 +1,3 @@
-# pyClEsperanto Import
-from ._version import VERSION as __version__
-from ._version import CLIC_VERSION as __clic_version__
 
 from ._core import (
     select_backend,
@@ -8,8 +5,9 @@ from ._core import (
     select_device,
     list_available_devices,
     list_available_backends,
+    default_initialisation,
 )
-from ._array import _Array, Image, is_image
+from ._array import Image, is_image
 from ._memory import create, create_like, push, pull
 from ._functionalities import imshow
 
@@ -19,16 +17,14 @@ from ._tier3 import *
 from ._tier4 import *
 from ._tier5 import *
 
-# Generic Import
-import os
+# # Generic Import
+# import os
 
-# Supports for ocl-icd find shipped OpenCL ICDs, cf.
-os.environ["PYCLESPERANTO_HOME"] = os.path.dirname(os.path.abspath(__file__))
+# # Supports for ocl-icd find shipped OpenCL ICDs, cf.
+# os.environ["PYCLESPERANTO_HOME"] = os.path.dirname(os.path.abspath(__file__))
 
-__backends__ = list_available_backends()
-if __backends__:
-    _ = select_backend(__backends__[-1])
-else:
-    print("No backends available, pyclesperanto is unlikely to run.")
+from ._version import VERSION as __version__
+from ._version import CLIC_VERSION as __clic_version__
+from ._version import COMMON_ALIAS as __common_alias__
 
-__common_alias__ = "cle"
+default_initialisation()

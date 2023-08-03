2b00f5b88e3974bff0ef9ac6434eed55a524f496
StRigaud <stephane.rigaud@pasteur.fr>
2023-08-03 11:40:16 +0200

    update test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,2 @@
 def test_import():
     import pyclesperanto as cle
-
-    # print(cle.list_available_backends())
-
-    # cle.select_backend(backend=cle.list_available_backends()[-1])
-    # print(cle.list_available_devices())
--- tests/test_tier6.py
+++
@@ -1,7 +0,0 @@
-# import pyclesperanto as cle
-# import numpy as np
-
-# data_3d = np.random.random((3,7,10))
-
-# def test_voronoi_otsu_labeling():
-#     cle.voronoi_otsu_labeling(input_image=data_3d, spot_sigma=1, outline_sigma=1)

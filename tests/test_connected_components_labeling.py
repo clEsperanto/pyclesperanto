fc4632584833d3a7b57f4b55ce73e16f0f2d02e9
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-11-22 19:07:00 +0100

    update test name and function

--- tests/test_connected_components_labeling.py
+++ tests/test_connected_components_labeling.py
@@ -5,7 +5,7 @@ import numpy as np
 def test_connected_components_labeling_box():
     gpu_input = cle.push(np.asarray([[1, 0, 1], [1, 0, 0], [0, 0, 1]]))
     gpu_reference = cle.push(np.asarray([[1, 0, 2], [1, 0, 0], [0, 0, 3]]))
-    gpu_output = cle.connected_components_labeling_box(gpu_input)
+    gpu_output = cle.connected_components_labeling(gpu_input)
     a = cle.pull(gpu_output)
     b = cle.pull(gpu_reference)
     assert np.array_equal(a, b)

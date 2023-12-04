28b8d99520bd8c2ad27738cf51321cb591cfa708
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-12-04 17:04:16 +0100

    add default value

--- tests/test_relabel_sequential.py
+++ tests/test_relabel_sequential.py
@@ -2,18 +2,18 @@ import pyclesperanto as cle
 import numpy as np
 
 
-# def test_relabel_sequential():
-#     gpu_input = cle.push(np.asarray([[1, 2, 3], [1, 6, 6], [7, 8, 9]]))
-#     gpu_output = cle.create_like(gpu_input)
+def test_relabel_sequential():
+    gpu_input = cle.push(np.asarray([[1, 2, 3], [1, 6, 6], [7, 8, 9]]))
+    gpu_output = cle.create_like(gpu_input)
 
-#     gpu_reference = cle.push(np.asarray([[1, 2, 3], [1, 4, 4], [5, 6, 7]]))
+    gpu_reference = cle.push(np.asarray([[1, 2, 3], [1, 4, 4], [5, 6, 7]]))
 
-#     cle.relabel_sequential(gpu_input, gpu_output)
+    cle.relabel_sequential(gpu_input, gpu_output, 2048)
 
-#     a = cle.pull(gpu_output)
-#     b = cle.pull(gpu_reference)
+    a = cle.pull(gpu_output)
+    b = cle.pull(gpu_reference)
 
-#     print(a)
-#     print(b)
+    print(a)
+    print(b)
 
-#     assert np.array_equal(a, b)
+    assert np.array_equal(a, b)

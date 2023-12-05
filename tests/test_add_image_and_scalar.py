748e4150a9ec3fa745568da2cfa2eaeb28de2aa7
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-12-05 17:32:20 +0100

    fix tests

--- tests/test_add_image_and_scalar.py
+++ tests/test_add_image_and_scalar.py
@@ -10,11 +10,11 @@ def test_add_image_and_scalar():
     # push an array to the GPU
     flip = cle.push(data)
     assert flip.shape == (10, 10)
-    assert isinstance(flip, cle.Image)
+    assert isinstance(flip, cle.Array)
     # create memory for the output
     flop = cle.create_like(data)
     assert flop.shape == (10, 10)
-    assert isinstance(flop, cle.Image)
+    assert isinstance(flop, cle.Array)
     # add a constant to all pixels
     cle.add_image_and_scalar(flip, flop, 100.0)
     # Note the transposition!

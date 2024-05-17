a6bd0a3337e4416a3817bde895a4773dfeda77ad
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-05-17 15:42:38 +0200

    refactor decorator

--- pyclesperanto/_decorators.py
+++ pyclesperanto/_decorators.py
@@ -1,6 +1,6 @@
 import inspect
 from functools import wraps
-from typing import Callable, Optional
+from typing import Any, Callable, List, Optional
 
 from toolz import curry
 
@@ -9,89 +9,86 @@ from ._core import Device
 from ._memory import push
 
 
+class CallableFunction:
+    """A class representing a callable function."""
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+class PluginFunction(CallableFunction):
+    """A class representing a plugin function."""
+
+    def __init__(
+        self,
+        function: Callable[..., Any],
+        category: Optional[List] = None,
+        priority: int = 0,
+    ):
+        self.function = function
+        self.fullargspec = inspect.getfullargspec(function)
+        self.category = category
+        self.priority = priority
+
+    def __call__(self, *args, **kwargs):
+        return self.function(*args, **kwargs)
+
+
 @curry
 def plugin_function(
-    function: Callable,
-    category: Optional[list] = None,
+    function: PluginFunction,
+    category: Optional[List] = None,
     priority: int = 0,
 ) -> Callable:
-    """Function decorator to ensure correct types and values of all parameters.
+    """A decorator for kernels functions."""
+    function = PluginFunction(function, category, priority)
 
-    The given input parameters are either of type OpenCL data/image/buffer (which the GPU
-    understands) or are converted to this type (see push function). If output
-    parameters of type Image are not set, an empty image is created and
-    handed over.
-
-    Parameters
-    ----------
-    function : callable
-        The function to be executed on the GPU.
-    output_creator : callable, optional
-        A function to create an output cleImage given an input cleImage. By
-        default, we create output images of the same shape and type as input
-        images.
-    device_selector : callable, optional
-        A function to select a device. By default, we use the current device instance.
-    category : list of str, optional
-        A list of category names the function is associated with
-    priority : int, optional
-        can be used in lists of multiple operations to differentiate multiple operations that fulfill the same purpose
-        but better/faster/more general.
-
-    Returns
-    -------
-    worker_function : callable
-        The actual function call that will be executed, magically creating
-        output arguments of the correct type.
-    """
-
-    function.fullargspec = inspect.getfullargspec(function)
-    function.category = category
-    function.priority = priority
-
-    @wraps(function)
+    @wraps(function.function)
     def worker_function(*args, **kwargs):
-        sig = inspect.signature(function)
-        # create mapping from position and keyword arguments to parameters
-        # will raise a TypeError if the provided arguments do not match the signature
-        # https://docs.python.org/3/library/inspect.html#inspect.Signature.bind
+        sig = inspect.signature(function.function)
         bound = sig.bind(*args, **kwargs)
-        # set default values for missing arguments
-        # https://docs.python.org/3/library/inspect.html#inspect.BoundArguments.apply_defaults
         bound.apply_defaults()
 
-        args_list = function.fullargspec.args
-        index = next(
-            (i for i, element in enumerate(args_list) if "input_image" in element), -1
-        )
-        arg_name = args_list[index]
+        arg_name = get_input_image_arg_name(function)
+        process_arguments(function, bound, arg_name)
 
-        # copy images to GPU, and create output array if necessary
-        for key, value in bound.arguments.items():
-            if (
-                is_image(value)
-                and key in sig.parameters
-                and sig.parameters[key].annotation is Image
-            ):
-                bound.arguments[key] = push(value)
-            if (
-                key in sig.parameters
-                and sig.parameters[key].annotation is Device
-                and value is None
-            ):
-                input_image = bound.arguments[arg_name]
-                bound.arguments[key] = input_image.device
-
-        # call the decorated function
-        result = function(*bound.args, **bound.kwargs)
-
-        # # Cast the result as an Array if it is not already
-        # if not isinstance(result, _Array):
-        #     result = _Array(result)
+        result = function.function(*bound.args, **bound.kwargs)
 
         return result
 
-    # this is necessary to obfuscate pyclesperanto's internal structure
     worker_function.__module__ = "pyclesperanto"
 
     return worker_function
+
+
+def get_input_image_arg_name(function: PluginFunction) -> str:
+    """Get the name of the input image argument."""
+    args_list = function.fullargspec.args
+    index = next(
+        (i for i, element in enumerate(args_list) if "input_image" in element), -1
+    )
+    if index == -1:
+        raise NotImplementedError(
+            f"Wrong usage of decorator for the function {function.function.__name__}"
+        )
+    return args_list[index]
+
+
+def process_arguments(
+    function: PluginFunction, bound: inspect.BoundArguments, input_image_arg_name: str
+):
+    sig = inspect.signature(function.function)
+    for key, value in bound.arguments.items():
+        if (
+            is_image(value)
+            and key in sig.parameters
+            and sig.parameters[key].annotation is Image
+        ):
+            bound.arguments[key] = push(value)
+        if (
+            key in sig.parameters
+            and sig.parameters[key].annotation is Optional[Device]
+            and value is None
+        ):
+            input_image = bound.arguments[input_image_arg_name]
+            bound.arguments[key] = input_image.device

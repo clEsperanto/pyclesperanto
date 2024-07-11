e268b8e5b9cc6d487703689613b648458dc7ebcb
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-07-11 18:36:20 +0200

    update version

--- pyclesperanto/_version.py
+++ pyclesperanto/_version.py
@@ -1,10 +1,10 @@
 # pyclesperanto version
-VERSION_CODE = 0, 11, 0
+VERSION_CODE = 0, 11, 1
 VERSION_STATUS = ""
 VERSION = ".".join(str(x) for x in VERSION_CODE) + VERSION_STATUS
 
 # clic version
-CLIC_VERSION_CODE = 0, 11, 0
+CLIC_VERSION_CODE = 0, 11, 1
 CLIC_VERSION_STATUS = ""
 CLIC_VERSION = ".".join(str(x) for x in CLIC_VERSION_CODE) + CLIC_VERSION_STATUS
 

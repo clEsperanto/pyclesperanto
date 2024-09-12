30d7953c9efa530c03b6d08093340d5a7ae4bf4e
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2024-09-12 11:51:57 +0200

    add doc

--- pyclesperanto/_categories.py
+++ pyclesperanto/_categories.py
@@ -1,6 +1,15 @@
 from inspect import getmembers, isfunction
- 
-def categories():   
+
+
+def categories() -> list:
+    """
+    Returns a list of all categories of operations in pyclesperanto
+
+    Returns
+    -------
+    list
+        A list of strings with category names
+    """
 
     import pyclesperanto as cle
 
@@ -8,7 +17,7 @@ def categories():
 
     cats = []
 
-    for operation_name, operation in ops:
+    for _, operation in ops:
         if hasattr(operation, "categories") and operation.categories is not None:
             for cat in operation.categories:
                 if cat not in cats:

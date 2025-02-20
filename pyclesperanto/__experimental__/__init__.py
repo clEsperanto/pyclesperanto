bca2b57ca3dc57a9cc4e4e1b1a4b6519610971e1
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-02-20 11:30:33 +0100

    demo test and experimental wrapper

--- pyclesperanto/__experimental__/__init__.py
+++ pyclesperanto/__experimental__/__init__.py
@@ -1,2 +1,5 @@
-from .._pyclesperanto import _fft_execute as fft_test
+from .._pyclesperanto import _convolution as convolution
+from .._pyclesperanto import _deconvolution as deconvolution
+from .._pyclesperanto import _fft_backward as ifft
+from .._pyclesperanto import _fft_forward as fft
 from .clahe import clahe
--- src/wrapper/fft.cpp
+++ src/wrapper/fft.cpp
@@ -6,7 +6,34 @@
 namespace py = pybind11;
 
 auto fft_(py::module &m) -> void {
-    m.def("_fft_execute", &cle::fft::fft_execute, "Call cle::fft::execute from C++ CLIc.",
-        py::return_value_policy::take_ownership,
-        py::arg("data"));
+
+    m.def("_fft_forward", &cle::fft::fft_forward, "Apply forward Fourier Transform",
+        // py::return_value_policy::take_ownership,
+        py::arg("input"),
+        py::arg("output")
+    );
+
+    m.def("_fft_backward", &cle::fft::fft_backward, "Apply backward Fourier Transform",
+        // py::return_value_policy::take_ownership,
+        py::arg("input"),
+        py::arg("output")
+    );
+
+    m.def("_convolution", &cle::fft::convolution, "Apply convolution",
+        // py::return_value_policy::take_ownership,
+        py::arg("input"),
+        py::arg("kernel"),
+        py::arg("output"),
+        py::arg("correlate")
+    );
+
+    m.def("_deconvolution", &cle::fft::deconvolution, "Apply deconvolution",
+        // py::return_value_policy::take_ownership,
+        py::arg("input"),
+        py::arg("kernel"),
+        py::arg("normal"),
+        py::arg("output"),
+        py::arg("iterations"),
+        py::arg("regularization")
+    );
 }

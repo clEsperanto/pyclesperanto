db99e121d9a4661a81bf6efe2d92841333ebd3b9
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-02-27 19:24:36 +0100

    test vkFFT

--- pyclesperanto/__experimental__/__init__.py
+++ pyclesperanto/__experimental__/__init__.py
@@ -1,5 +1,9 @@
-from .._pyclesperanto import _convolution as convolution
-from .._pyclesperanto import _deconvolution as deconvolution
-from .._pyclesperanto import _fft_backward as ifft
-from .._pyclesperanto import _fft_forward as fft
+from .._pyclesperanto import _convolution as cl_convolution
+from .._pyclesperanto import _deconvolution as cl_deconvolution
+from .._pyclesperanto import _fft_backward as cl_ifft
+from .._pyclesperanto import _fft_forward as cl_fft
+from .._pyclesperanto import _performConvolution as vk_convolution
+from .._pyclesperanto import _performDeconvolution as vk_deconvolution
+from .._pyclesperanto import _performFFT as vk_fft
+from .._pyclesperanto import _performIFFT as vk_ifft
 from .clahe import clahe
--- src/wrapper/fft.cpp
+++ src/wrapper/fft.cpp
@@ -7,19 +7,19 @@ namespace py = pybind11;
 
 auto fft_(py::module &m) -> void {
 
-    m.def("_fft_forward", &cle::fft::fft_forward, "Apply forward Fourier Transform",
+    m.def("_fft_forward", &cle::fft::fft_forward, "Apply forward Fourier Transform (clFFT)",
         // py::return_value_policy::take_ownership,
         py::arg("input"),
         py::arg("output")
     );
 
-    m.def("_fft_backward", &cle::fft::fft_backward, "Apply backward Fourier Transform",
+    m.def("_fft_backward", &cle::fft::fft_backward, "Apply backward Fourier Transform (clFFT)",
         // py::return_value_policy::take_ownership,
         py::arg("input"),
         py::arg("output")
     );
 
-    m.def("_convolution", &cle::fft::convolution, "Apply convolution",
+    m.def("_convolution", &cle::fft::convolution, "Apply convolution (clFFT)",
         // py::return_value_policy::take_ownership,
         py::arg("input"),
         py::arg("kernel"),
@@ -27,7 +27,37 @@ auto fft_(py::module &m) -> void {
         py::arg("correlate")
     );
 
-    m.def("_deconvolution", &cle::fft::deconvolution, "Apply deconvolution",
+    m.def("_deconvolution", &cle::fft::deconvolution, "Apply deconvolution (clFFT)",
+        // py::return_value_policy::take_ownership,
+        py::arg("input"),
+        py::arg("kernel"),
+        py::arg("normal"),
+        py::arg("output"),
+        py::arg("iterations"),
+        py::arg("regularization")
+    );
+
+    m.def("_performFFT", &cle::fft::performFFT, "Apply forward Fourier Transform (vkFFT)",
+        // py::return_value_policy::take_ownership,
+        py::arg("input"),
+        py::arg("output")
+    );
+
+    m.def("_performIFFT", &cle::fft::performIFFT, "Apply backward Fourier Transform (vkFFT)",
+        // py::return_value_policy::take_ownership,
+        py::arg("input"),
+        py::arg("output")
+    );
+
+    m.def("_performConvolution", &cle::fft::performConvolution, "Apply convolution (vkFFT)",
+        // py::return_value_policy::take_ownership,
+        py::arg("input"),
+        py::arg("kernel"),
+        py::arg("output"),
+        py::arg("correlate")
+    );
+
+    m.def("_performDeconvolution", &cle::fft::performDeconvolution, "Apply deconvolution (vkFFT)",
         // py::return_value_policy::take_ownership,
         py::arg("input"),
         py::arg("kernel"),

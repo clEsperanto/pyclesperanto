f023fac8150510aac1277c842b0a43eeb3afeefc
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-03-04 18:56:56 +0100

    update python test of latest changes in fft

--- pyclesperanto/__experimental__/__init__.py
+++ pyclesperanto/__experimental__/__init__.py
@@ -1,5 +1,5 @@
-from .._pyclesperanto import _convolution as cl_convolution
-from .._pyclesperanto import _deconvolution as cl_deconvolution
+# from .._pyclesperanto import _convolution as cl_convolution
+# from .._pyclesperanto import _deconvolution as cl_deconvolution
 from .._pyclesperanto import _fft_backward as cl_ifft
 from .._pyclesperanto import _fft_forward as cl_fft
 from .._pyclesperanto import _performConvolution as vk_convolution
@@ -7,3 +7,8 @@ from .._pyclesperanto import _performDeconvolution as vk_deconvolution
 from .._pyclesperanto import _performFFT as vk_fft
 from .._pyclesperanto import _performIFFT as vk_ifft
 from .clahe import clahe
+
+# from .._pyclesperanto import _fft_complex_multiply as fft_complex_multiply
+# from .._pyclesperanto import _fft_complex_conju_multiply as fft_complex_conju_multiply
+# from .._pyclesperanto import _fft_real_divide as fft_real_divide
+# from .._pyclesperanto import _fft_real_multiply as fft_real_multiply
--- src/wrapper/fft.cpp
+++ src/wrapper/fft.cpp
@@ -5,37 +5,78 @@
 
 namespace py = pybind11;
 
+
 auto fft_(py::module &m) -> void {
 
     m.def("_fft_forward", &cle::fft::fft_forward, "Apply forward Fourier Transform (clFFT)",
-        // py::return_value_policy::take_ownership,
+        py::return_value_policy::take_ownership,
         py::arg("input"),
         py::arg("output")
     );
 
     m.def("_fft_backward", &cle::fft::fft_backward, "Apply backward Fourier Transform (clFFT)",
-        // py::return_value_policy::take_ownership,
+        py::return_value_policy::take_ownership,
         py::arg("input"),
         py::arg("output")
     );
 
-    m.def("_convolution", &cle::fft::convolution, "Apply convolution (clFFT)",
-        // py::return_value_policy::take_ownership,
-        py::arg("input"),
-        py::arg("kernel"),
-        py::arg("output"),
-        py::arg("correlate")
-    );
+    // m.def("_convolution", &cle::fft::convolution, "Apply convolution (clFFT)",
+    //     py::return_value_policy::take_ownership,
+    //     py::arg("input"),
+    //     py::arg("kernel"),
+    //     py::arg("output"),
+    //     py::arg("correlate")
+    // );
 
-    m.def("_deconvolution", &cle::fft::deconvolution, "Apply deconvolution (clFFT)",
-        // py::return_value_policy::take_ownership,
-        py::arg("input"),
-        py::arg("kernel"),
-        py::arg("normal"),
-        py::arg("output"),
-        py::arg("iterations"),
-        py::arg("regularization")
-    );
+    // m.def("_deconvolution", &cle::fft::deconvolution, "Apply deconvolution (clFFT)",
+    //     py::return_value_policy::take_ownership,
+    //     py::arg("input"),
+    //     py::arg("kernel"),
+    //     py::arg("normal"),
+    //     py::arg("output"),
+    //     py::arg("iterations"),
+    //     py::arg("regularization")
+    // );
+
+
+
+    // m.def("_fft_complex_multiply", &complex_multiply, "Complex multiply",
+    //     py::return_value_policy::take_ownership,
+    //     py::arg("input"),
+    //     py::arg("psf"),
+    //     py::arg("output")
+    // );
+
+    // m.def("_fft_complex_conju_multiply", &complex_conju_multiply, "Complex conjugate multiply",
+    //     py::return_value_policy::take_ownership,
+    //     py::arg("input"),
+    //     py::arg("psf"),
+    //     py::arg("output")
+    // );
+
+    // m.def("_fft_real_divide", &real_divide, "Real divide",
+    //     py::return_value_policy::take_ownership,
+    //     py::arg("input"),
+    //     py::arg("psf"),
+    //     py::arg("output")
+    // );
+
+    // m.def("_fft_real_multiply", &real_multiply, "Real multiply",
+    //     py::return_value_policy::take_ownership,
+    //     py::arg("input"),
+    //     py::arg("psf"),
+    //     py::arg("output")
+    // );
+
+
+
+
+
+
+
+
+
+
 
     m.def("_performFFT", &cle::fft::performFFT, "Apply forward Fourier Transform (vkFFT)",
         // py::return_value_policy::take_ownership,

74d22d1dd24820267fed4726872f8899a38e4c48
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2025-07-04 11:25:06 +0200

    clean up

--- pyclesperanto/__experimental__/__init__.py
+++ pyclesperanto/__experimental__/__init__.py
@@ -1,5 +1,4 @@
 from .clahe import clahe  # we keep this import for example purposes
 from .gaussian_derivative import gaussian_derivative
 from .hessian_gaussian_eigenvalues import hessian_gaussian_eigenvalues
-from .harris_corner import harris_corner
 from .sato import sato

fix tests for gradient and hessian

tests/test_gradient_x.py
@@ -15,23 +15,22 @@ def test_gradient_x():
                 [0, 1, 3, 0, 0],
                 [0, 0, 0, 0, 0],
             ]
-        )
+        ).astype(float)
     )
 
     reference = cle.push(
         np.asarray(
             [
-                [0, 0, 0, 0, 0],
-                [-1, -2, 1, 2, 0],
-                [-1, -2, 1, 2, 0],
-                [-1, -3, 1, 3, 0],
-                [0, 0, 0, 0, 0],
+                [0.0, 0.0, 0.0, 0.0, 0.0],
+                [1.0, 1.0, -0.5, -1.0, 0.0],
+                [1.0, 1.0, -0.5, -1.0, 0.0],
+                [1.0, 1.5, -0.5, -1.5, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0],
             ]
         )
     )
 
-    result = cle.create(test)
-    cle.gradient_x(test, result)
+    result = cle.gradient_x(test)
 
     a = cle.pull(result)
     b = cle.pull(reference)

tests/test_gradient_y.py
@@ -21,17 +21,16 @@ def test_gradient_y():
     reference = cle.push(
         np.asarray(
             [
-                [0, -1, -2, 0, 0],
-                [0, -1, -2, 0, 0],
-                [0, 0, -1, 0, 0],
-                [0, 1, 2, 0, 0],
-                [0, 1, 3, 0, 0],
+                [0.0, 1.0, 2.0, 0.0, 0.0],
+                [0.0, 0.5, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 0.5, 0.0, 0.0],
+                [0.0, -0.5, -1.0, 0.0, 0.0],
+                [0.0, -1.0, -3.0, 0.0, 0.0],
             ]
         )
     )
 
-    result = cle.create(test)
-    cle.gradient_y(test, result)
+    result = cle.gradient_y(test)
 
     a = cle.pull(result)
     b = cle.pull(reference)
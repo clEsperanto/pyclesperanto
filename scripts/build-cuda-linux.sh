9bbe4b1282293b31b6e18b6e10bc3747d5b989f2
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2026-03-12 21:48:10 +0100

    update deployement ci

---
+++ scripts/build-cuda-linux.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+CUDA_VERSION="12-6"
+
+set -e -x
+
+# Install CUDA toolkit in manylinux container (AlmaLinux 8 / CentOS 8)
+yum install -y yum-utils
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+
+yum install -y \
+    cuda-nvcc-${CUDA_VERSION} \
+    cuda-cudart-devel-${CUDA_VERSION} \
+    cuda-nvrtc-devel-${CUDA_VERSION} \
+    libnvjitlink-devel-${CUDA_VERSION}
+
+# Create stub symlink for linking
+ln -sf /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
+
+# Append license information
+echo "pyclesperanto-cuda wheel includes NVIDIA CUDA toolkit components subject to the NVIDIA CUDA EULA" >>"${SCRIPT_DIR}/../LICENSE"

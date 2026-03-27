61e50f10b9400c46752d9132c1b57f4c41a6813a
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2026-03-27 15:10:43 +0100

    add tests

--- tests/array/test_array_device.py
+++ tests/array/test_array_device.py
@@ -6,7 +6,7 @@ import pyclesperanto as cle
 
 @pytest.mark.backend
 def test_zeros_arrays(gpu_backend):
-    gpu_arr = cle.Array.zeros((10, 10))
+    gpu_arr = cle.zeros((10, 10))
 
     assert gpu_arr.shape == (10, 10)
     assert gpu_arr.sum() == 0
@@ -17,7 +17,7 @@ def test_zeros_arrays(gpu_backend):
 @pytest.mark.backend
 def test_empty_like(gpu_backend):
     arr = np.zeros((10, 10))
-    gpu_arr = cle.Array.empty_like(arr)
+    gpu_arr = cle.empty_like(arr)
 
     assert gpu_arr.shape == arr.shape
 
@@ -25,29 +25,37 @@ def test_empty_like(gpu_backend):
 @pytest.mark.backend
 def test_zeros_like(gpu_backend):
     arr = np.zeros((10, 10))
-    gpu_arr = cle.Array.zeros_like(arr)
+    gpu_arr = cle.zeros_like(arr)
 
     assert gpu_arr.shape == arr.shape
     assert gpu_arr.sum() == 0
     assert gpu_arr.max() == 0
     assert gpu_arr.min() == 0
-
 
 @pytest.mark.backend
-def test_to_device(gpu_backend):
+def test_ones_like(gpu_backend):
     arr = np.zeros((10, 10))
-    gpu_arr = cle.Array.to_device(arr)
+    gpu_arr = cle.ones_like(arr)
 
     assert gpu_arr.shape == arr.shape
-    assert gpu_arr.sum() == 0
-    assert gpu_arr.max() == 0
-    assert gpu_arr.min() == 0
+    assert gpu_arr.sum() == 100
+    assert gpu_arr.max() == 1
+    assert gpu_arr.min() == 1
+
+@pytest.mark.backend
+def test_ones_arrays(gpu_backend):
+    gpu_arr = cle.ones((10, 10))
+
+    assert gpu_arr.shape == (10, 10)
+    assert gpu_arr.sum() == 100
+    assert gpu_arr.max() == 1
+    assert gpu_arr.min() == 1
 
 
 @pytest.mark.backend
 def test_from_array(gpu_backend):
     arr = np.zeros((10, 10))
-    gpu_arr = cle.Array.from_array(arr)
+    gpu_arr = cle.from_array(arr)
 
     assert gpu_arr.shape == arr.shape
     assert gpu_arr.sum() == 0
@@ -67,7 +75,7 @@ def test_array_ufunc_add(gpu_backend):
 
 @pytest.mark.backend
 def test_repr(gpu_backend):
-    array = cle.Array.zeros((10, 10))
+    array = cle.zeros((10, 10))
     result = repr(array)
 
     assert len(result) > 0
